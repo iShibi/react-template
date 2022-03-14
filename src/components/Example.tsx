dc1c21cccc93799fc87b1adc49d22583d4e60ff8
Shubham Parihar <shubhamparihar391@gmail.com>
2022-03-14 12:20:18 +0530

    chore: add eslint

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -9,10 +9,10 @@ export function Example() {
 		<div className='flex flex-col items-center gap-6 py-4 font-mono'>
 			<span className='px-6 text-9xl font-bold'>{value}</span>
 			<div className='flex flex-row gap-x-2 text-8xl text-white'>
-				<button className='bg-red-500 py-4 px-8' onClick={e => dispatch(decrement())} disabled={value <= -69}>
+				<button className='bg-red-500 py-4 px-8' onClick={() => dispatch(decrement())} disabled={value <= -69}>
 					-
 				</button>
-				<button className='bg-blue-500 py-4 px-8' onClick={e => dispatch(increment())} disabled={value >= 69}>
+				<button className='bg-blue-500 py-4 px-8' onClick={() => dispatch(increment())} disabled={value >= 69}>
 					+
 				</button>
 			</div>
